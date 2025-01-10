cf4f6b688ae2c3d112a6247bc212da55c3240d06
RamkumarV09 <ramkumarv0986@gmail.com>
2025-01-10 12:19:11 +0530

    My First Code

---
+++ src/org/sample/SampleGIT.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class SampleGIT {
+	
+	private void meth1() {
+
+		System.out.println("Method 1");
+	}
+	
+	private void meth2() {
+
+		System.out.println("Method2");
+	}
+	
+	private void meth3() {
+
+		System.out.println("Method3");
+	}
+
+}
